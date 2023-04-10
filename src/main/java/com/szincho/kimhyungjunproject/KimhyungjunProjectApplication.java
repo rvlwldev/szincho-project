d3aec0f99d5e3990e1244ac973ca416d405f586c
rvlwldev <rvlwlasd77@gmail.com>
2023-04-10 12:15:23 +0900

    init project

---
+++ src/main/java/com/szincho/kimhyungjunproject/KimhyungjunProjectApplication.java
@@ -0,0 +1,13 @@
+package com.szincho.kimhyungjunproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimhyungjunProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimhyungjunProjectApplication.class, args);
+	}
+
+}
