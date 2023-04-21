e889b9c1d1ff7bd7a4aa81bcd5d6ca388ce875fd
rvlwldev <rvlwlasd77@gmail.com>
2023-04-21 14:58:39 +0900

    feat: Food 엔티티에 필수값 체크

--- src/main/java/com/szincho/kimhyungjunproject/Food/Entity/Food.java
+++ src/main/java/com/szincho/kimhyungjunproject/Food/Entity/Food.java
@@ -1,19 +1,34 @@
 package com.szincho.kimhyungjunproject.Food.Entity;
 
+import com.szincho.kimhyungjunproject.Food.DTO.FoodDTO;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Food {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
+
+    @NotNull(message = "음식이름은 필수값 입니다.")
     String name;
+
+    @NotNull(message = "가격은 필수값 입니다.")
     long price;
+
     String description;
+
+    public Food (FoodDTO dto){
+        this.name = dto.getName();
+        this.price = dto.getPrice();
+        this.description = dto.getDescription();
+    }
 }
