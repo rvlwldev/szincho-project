67f561646b835d6bbc3f70b627dba59882dfc7b3
rvlwldev <rvlwlasd77@gmail.com>
2023-04-27 19:33:29 +0900

    refactor: Mapper 사용을 위해 @Builder, @AllArgsConstructor 추가, @Setter 및 @NoArgsConstructor 추가

--- src/main/java/com/szincho/kimhyungjunproject/Food/DTO/FoodDTO.java
+++ src/main/java/com/szincho/kimhyungjunproject/Food/DTO/FoodDTO.java
@@ -1,8 +1,12 @@
 package com.szincho.kimhyungjunproject.Food.DTO;
 
-import lombok.Getter;
+import lombok.*;
 
 @Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FoodDTO {
     String name;
     long price;
