7af7f9e870e7ed3018fe1c42b505e4ce16753990
rvlwldev <rvlwlasd77@gmail.com>
2023-04-29 19:22:39 +0900

    feat: Order 엔티티에 주문시간 추가

--- src/main/java/com/szincho/kimhyungjunproject/Order/Entity/Order.java
+++ src/main/java/com/szincho/kimhyungjunproject/Order/Entity/Order.java
@@ -5,8 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -25,6 +27,9 @@ public class Order {
     @ManyToMany
     @JoinColumn(name = "food_id")
     List<Food> foods;
+
+    @CreationTimestamp
+    private LocalDateTime orderTime;
 
     boolean isArrived;
     boolean isDeparted;
