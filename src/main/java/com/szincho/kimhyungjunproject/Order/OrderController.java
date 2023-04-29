50fb20a3767f6b6769ecb234b636102e1dccd684
rvlwldev <rvlwlasd77@gmail.com>
2023-04-29 15:20:58 +0900

    feat: 주문 기능 구현

--- src/main/java/com/szincho/kimhyungjunproject/Order/OrderController.java
+++ src/main/java/com/szincho/kimhyungjunproject/Order/OrderController.java
@@ -1,18 +1,44 @@
 package com.szincho.kimhyungjunproject.Order;
 
+import com.szincho.kimhyungjunproject.Food.Exception.FoodNotFoundException;
+import com.szincho.kimhyungjunproject.Food.FoodService;
+import com.szincho.kimhyungjunproject.Order.DTO.Mapper.OrderRequestMapper;
+import com.szincho.kimhyungjunproject.Order.DTO.Request.OrderRequest;
+import com.szincho.kimhyungjunproject.Order.DTO.Response.OrderResponse;
+import com.szincho.kimhyungjunproject.Order.Entity.Order;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.validation.Valid;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/orders")
 public class OrderController {
 
     private final OrderService service;
-    private final OrderMapper mapper;
+    private final FoodService foodService;
 
     @Autowired
-    public OrderController(OrderService service, OrderMapper mapper) {
+    public OrderController(OrderService service, FoodService foodService) {
         this.service = service;
-        this.mapper = mapper;
+        this.foodService = foodService;
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderResponse> receiveOrder(@RequestBody @Valid OrderRequest request) throws FoodNotFoundException {
+        HashMap<Integer, Integer>  CountWithFoodMap = OrderRequestMapper.MAPPER.getCountWithFoodMap(request);
+        Order order = OrderRequestMapper.MAPPER.getOrderByRequestedDto(request, foodService);
+
+        OrderResponse response = service.saveOrder(order, CountWithFoodMap);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(response);
     }
 
 }
--- src/main/java/com/szincho/kimhyungjunproject/Order/OrderService.java
+++ src/main/java/com/szincho/kimhyungjunproject/Order/OrderService.java
@@ -1,7 +1,29 @@
 package com.szincho.kimhyungjunproject.Order;
 
+import com.szincho.kimhyungjunproject.Food.Exception.FoodNotFoundException;
+import com.szincho.kimhyungjunproject.Order.DTO.Mapper.OrderResponseMapper;
+import com.szincho.kimhyungjunproject.Order.DTO.Response.OrderResponse;
+import com.szincho.kimhyungjunproject.Order.Entity.Order;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service
 public class OrderService {
+
+    private final OrderRepository orderRepo;
+
+    public OrderService(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
+
+    public OrderResponse saveOrder(Order order, HashMap<Integer, Integer> CountWithFoodMap) throws FoodNotFoundException {
+        validateOrderFoods(order);
+        return OrderResponseMapper.MAPPER.toDto(orderRepo.save(order), CountWithFoodMap);
+    }
+
+    private void validateOrderFoods(Order order) throws FoodNotFoundException {
+
+    }
+
 }
