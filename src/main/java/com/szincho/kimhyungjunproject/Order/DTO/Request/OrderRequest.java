5dd135c90940023c2130481068b671704978c00c
rvlwldev <rvlwlasd77@gmail.com>
2023-04-29 16:25:03 +0900

    feat: 클라이언트 입력값에 대한 유효성 검사 어노테이션 추가

--- src/main/java/com/szincho/kimhyungjunproject/Order/DTO/Request/OrderRequest.java
+++ src/main/java/com/szincho/kimhyungjunproject/Order/DTO/Request/OrderRequest.java
@@ -2,6 +2,7 @@ package com.szincho.kimhyungjunproject.Order.DTO.Request;
 
 import lombok.Data;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.List;
@@ -11,7 +12,7 @@ public class OrderRequest {
     @NotNull(message = "배달주소를 입력해주세요.")
     String destination;
 
-    @NotNull(message = "주문 음식을 골라주세요.")
     @NotEmpty(message = "주문 음식을 골라주세요.")
+    @Valid
     List<OrderedFoodsRequest> foods;
 }
--- src/main/java/com/szincho/kimhyungjunproject/Order/DTO/Request/OrderedFoodsRequest.java
+++ src/main/java/com/szincho/kimhyungjunproject/Order/DTO/Request/OrderedFoodsRequest.java
@@ -2,8 +2,13 @@ package com.szincho.kimhyungjunproject.Order.DTO.Request;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+
 @Data
 public class OrderedFoodsRequest {
+    @Min(value = 1, message = "음식을 선택해주세요.")
     int id;
+
+    @Min(value = 1, message = "1개 이상 주문이 가능합니다.")
     int count;
 }
