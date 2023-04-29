0e382ee9510f1c701090d0f81e57ea1d0f9e04a3
rvlwldev <rvlwlasd77@gmail.com>
2023-04-29 19:23:36 +0900

    feat: 주문 히스토리 Entity -> DTO 맵핑 로직 구현

--- src/main/java/com/szincho/kimhyungjunproject/Order/DTO/Mapper/OrderResponseMapper.java
+++ src/main/java/com/szincho/kimhyungjunproject/Order/DTO/Mapper/OrderResponseMapper.java
@@ -5,15 +5,57 @@ import com.szincho.kimhyungjunproject.Order.DTO.Response.OrderFoodResponse;
 import com.szincho.kimhyungjunproject.Order.DTO.Response.OrderResponse;
 import com.szincho.kimhyungjunproject.Order.Entity.Order;
 import com.szincho.kimhyungjunproject._Global.Interface.DataMapper;
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring", uses = DataMapper.class)
 public interface OrderResponseMapper extends DataMapper<OrderResponse, Order> {
 
     OrderResponseMapper MAPPER = Mappers.getMapper(OrderResponseMapper.class);
+
+    @Mapping(source = "foods", target = "foods", qualifiedByName = "toOrderResponseList")
+    OrderResponse toDto(final Order entity);
+
+    @Named("toOrderResponseList")
+    default List<OrderFoodResponse> toOrderResponseList(List<Food> foods) {
+        HashMap<Integer, Integer> countsWithFood = getCountWithFoodMap(foods);
+
+        return Set.copyOf(foods).stream()
+                .map(food -> toOrderResponse(food, countsWithFood.get(food.getId())))
+                .collect(Collectors.toList());
+    }
+
+    @AfterMapping
+    default void afterMapping(@MappingTarget OrderResponse dto) {
+        int totalPrice = 0;
+        for (OrderFoodResponse food : dto.getFoods()) totalPrice += food.getPrice() * food.getCount();
+
+        dto.setTotalPrice(totalPrice);
+    }
+
+    default OrderFoodResponse toOrderResponse(Food food, int count) {
+        return OrderFoodResponse.builder()
+                .name(food.getName())
+                .price(food.getPrice())
+                .count(count)
+                .build();
+    }
+
+    default HashMap<Integer, Integer> getCountWithFoodMap(List<Food> foods) {
+        HashMap<Integer, Integer> countsWithFood = new HashMap<>();
+
+        for (Food orderedFood : foods) {
+            int count = countsWithFood.getOrDefault(orderedFood.getId(), 0);
+
+            countsWithFood.put(orderedFood.getId(), count + 1);
+        }
+        return countsWithFood;
+    }
 
     default OrderResponse toDto(Order order, HashMap<Integer, Integer> countsWithFood) {
         OrderResponse response = OrderFoodResponse(order, countsWithFood);
@@ -28,6 +70,7 @@ public interface OrderResponseMapper extends DataMapper<OrderResponse, Order> {
 
         return response;
     }
+
     default OrderResponse OrderFoodResponse(Order order, HashMap<Integer, Integer> countsWithFood) {
         OrderResponse response = new OrderResponse();
 
