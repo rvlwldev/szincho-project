147ce4852ef178d1fce28450ebabd16881392f7e
rvlwldev <rvlwlasd77@gmail.com>
2023-05-01 19:51:27 +0900

    docs: OrderController에 대한 Swagger 문서화

--- src/main/java/com/szincho/kimhyungjunproject/Order/OrderController.java
+++ src/main/java/com/szincho/kimhyungjunproject/Order/OrderController.java
@@ -9,6 +9,8 @@ import com.szincho.kimhyungjunproject.Order.DTO.Request.OrderRequest;
 import com.szincho.kimhyungjunproject.Order.DTO.Response.OrderResponse;
 import com.szincho.kimhyungjunproject.Order.Entity.Order;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -64,18 +66,45 @@ public class OrderController {
         return service.getAllOrderHistoryDESC();
     }
 
+    @Operation(summary = "주문 취소", tags = "Order", description = "주문 취소 (해당 주문의 배달이 출발하기 전에 가능)")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "성공적으로 주문 취소 요청"),
+            @ApiResponse(responseCode = "400", description = "이미 배달이 출발했거나 배달이 완료된 주문일 경우"),
+            @ApiResponse(responseCode = "404", description = "해당 주문을 확인할 수 없을 때")
+    })
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "음식 아이디", required = true, dataType = "int", paramType = "query")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<OrderResponse> cancelOrder(@PathVariable("id") long id) throws IllegalOrderException, OrderNotFoundException {
         service.cancelOrder(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "주문 출발 처리", tags = "Order", description = "해당 주문의 배달 시작 처리")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "성공적으로 주문 배달 요청"),
+            @ApiResponse(responseCode = "400", description = "이미 배달이 출발했거나 배달이 완료된 주문일 경우"),
+            @ApiResponse(responseCode = "404", description = "해당 주문을 확인할 수 없을 때")
+    })
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "음식 아이디", required = true, dataType = "int", paramType = "query")
+    })
     @PatchMapping("/depart/{id}")
     public ResponseEntity<OrderResponse> departOrder(@PathVariable("id") long id) throws IllegalOrderException, OrderNotFoundException {
         service.departOrder(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "주문 도착 처리", tags = "Order", description = "해당 주문의 도착 처리")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "성공적으로 배달 도착 요청"),
+            @ApiResponse(responseCode = "400", description = "이미 배달이 출발했거나 배달이 완료된 주문일 경우"),
+            @ApiResponse(responseCode = "404", description = "해당 주문을 확인할 수 없을 때")
+    })
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "음식 아이디", required = true, dataType = "int", paramType = "query")
+    })
     @PatchMapping("/arrive/{id}")
     public ResponseEntity<OrderResponse> arriveOrder(@PathVariable("id") long id) throws IllegalOrderException, OrderNotFoundException {
         service.arriveOrder(id);
