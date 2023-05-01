7c5097ed5bcd6f0ea80a4d8761019b7317bec23a
rvlwldev <rvlwlasd77@gmail.com>
2023-05-01 20:01:17 +0900

    docs: 주문요청시, 반환정보에 대한 Swagger 문서화

--- src/main/java/com/szincho/kimhyungjunproject/Order/DTO/Response/OrderFoodResponse.java
+++ src/main/java/com/szincho/kimhyungjunproject/Order/DTO/Response/OrderFoodResponse.java
@@ -1,12 +1,21 @@
 package com.szincho.kimhyungjunproject.Order.DTO.Response;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@ApiModel(description = "주문 요청한 음식의 DTO")
 public class OrderFoodResponse {
+
+    @ApiModelProperty(value = "주문한 음식의 이름")
     String name;
+
+    @ApiModelProperty(value = "주문한 음식의 수량")
     int count;
+
+    @ApiModelProperty(value = "주문한 음식의 가격 (음식의 가격 * 수량)")
     long price;
 }
