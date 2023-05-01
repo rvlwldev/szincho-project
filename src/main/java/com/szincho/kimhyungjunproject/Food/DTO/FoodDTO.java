6571491059ac09a0111f96417315ba7ce5aeef13
rvlwldev <rvlwlasd77@gmail.com>
2023-05-01 19:26:47 +0900

    docs: Food DTO 에 관한 swagger 설명 추가

--- src/main/java/com/szincho/kimhyungjunproject/Food/DTO/FoodDTO.java
+++ src/main/java/com/szincho/kimhyungjunproject/Food/DTO/FoodDTO.java
@@ -1,5 +1,7 @@
 package com.szincho.kimhyungjunproject.Food.DTO;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.validation.constraints.Min;
@@ -10,14 +12,22 @@ import javax.validation.constraints.NotNull;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel(description = "음식 정보 DTO")
 public class FoodDTO {
 
+    @ApiModelProperty(value = "음식 이름", required = true)
     @NotNull(message = "음식이름을 입력하세요")
     String name;
 
+    @ApiModelProperty(
+            value = "음식의 가격, 등록 시, 1원 단위의 금액은 자동 '절삭' 됩니다. 최소금액은 1000원 입니다.",
+            required = true)
     @Min(value = 1000, message = "최소금액은 1000원 이상입니다.")
     long price;
 
+    @ApiModelProperty(
+            value = "음식의 관한 자세한 설명",
+            example = "직접 키우고 튀긴 치킨입니다")
     String description;
 
     public void setPrice(long price) {
