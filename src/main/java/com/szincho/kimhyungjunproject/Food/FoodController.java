20e3851589b35b23e2de1bc6897da87d88c46532
rvlwldev <rvlwlasd77@gmail.com>
2023-05-01 18:57:38 +0900

    docs: FoodController에 대한 Swagger 문서화

--- src/main/java/com/szincho/kimhyungjunproject/Food/FoodController.java
+++ src/main/java/com/szincho/kimhyungjunproject/Food/FoodController.java
@@ -39,11 +39,21 @@ public class FoodController {
         return service.getAllFoods();
     }
 
+    @Operation(summary = "선택된 음식 정보 조회", tags = "Food")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회성공"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 음식 ID 조회 시")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<FoodDTO> getFood(@PathVariable("id") int id) throws FoodNotFoundException {
         return ResponseEntity.ok().body(service.getFood(id));
     }
 
+    @Operation(summary = "음식 등록", tags = "Food")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "등록성공"),
+            @ApiResponse(responseCode = "400", description = "FoodDTO의 필수 값/조건이 누락되었을 경우")
+    })
     @PostMapping
     public ResponseEntity<FoodDTO> createFood(@RequestBody @Valid FoodDTO dto) {
         dto = service.saveFood(mapper.toEntity(dto));
@@ -51,12 +61,22 @@ public class FoodController {
         return ResponseEntity.status(HttpStatus.CREATED).body(dto);
     }
 
+    @Operation(summary = "음식 등록", tags = "Food")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "수정성공"),
+            @ApiResponse(responseCode = "400", description = "FoodDTO의 필수 값/조건이 누락되었을 경우, 수정 시 모든 값이 입력되어야함")
+    })
     @PatchMapping("/{id}")
     public ResponseEntity<FoodDTO> updateFood(@PathVariable("id") int id, @RequestBody @Valid FoodDTO dto) throws FoodNotFoundException {
         FoodDTO updated = service.updateFood(id, mapper.toEntity(dto));
         return ResponseEntity.ok().body(updated);
     }
 
+    @Operation(summary = "음식 정보 삭제", tags = "Food")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "삭제성공"),
+            @ApiResponse(responseCode = "404", description = "삭제 대상 음식의 ID이 존재하지 않을 경우")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteFood(@PathVariable("id") int id) throws FoodNotFoundException {
         service.deleteFood(id);
